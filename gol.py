dddc20840bb762c24a119081c5bf759adde3e05e
Zhang Yu <zhangyu@dingxiang-inc.com>
2019-06-27 20:17:10 +0800

    update python version

--- gol.py
+++ gol.py
@@ -5,30 +5,30 @@ import json
 from bottle import get, run, response, static_file
 
 
-global players
+_context = {'players': []}
+
 
 @get('/gol')
 def players():
     response.set_header('Content-Type', 'application/json')
-    global players
-    players = get_next_round_players(players)
-    return json.dumps(players.tolist())
+    _context['players'] = get_next_round_players(_context['players'])
+    return json.dumps(_context['players'].tolist())
 
 
 @get('/<row>,<col>')
 def index(row, col):
-    global players
-    players = np.random.rand(int(row), int(col)).round()
+    _context['players'] = np.random.rand(int(row), int(col)).round()
     return static_file('index.html', os.path.abspath(os.path.dirname(__file__)))
+
 
 def get_next_round_players(players):
     data = np.array(players)
     next_players = np.empty(data.shape).astype(int)
     cord_list = itertools.product(*map(range, data.shape))
     row, col = data.shape
-    for x,y in cord_list:
-        nb_sum = data[max(0,x-1):min(x+2,row), max(0,y-1):min(y+2,col)].sum() - data[x][y]
-        next_players[x][y] = (data[x][y], nb_sum) in ((1,2),(1,3),(0,3)) and 1 or 0
+    for x, y in cord_list:
+        nb_sum = data[max(0, x-1):min(x+2, row), max(0, y-1):min(y+2, col)].sum() - data[x][y]
+        next_players[x][y] = (data[x][y], nb_sum) in ((1, 2), (1, 3), (0, 3)) and 1 or 0
 
     return next_players
 
