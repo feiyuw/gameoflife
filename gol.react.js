c22e9878f78c648548e335eba575d2fa0f709792
Zhang Yu <zhangyu@dingxiang-inc.com>
2019-06-27 23:21:19 +0800

    update react version

--- gol.react.js
+++ gol.react.js
@@ -1,171 +1,133 @@
 /*
  * Game of life ReactJS version
  */
-
-'use strict';
-
+'use strict'
 
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import _ from 'lodash'
 
 
-function GameOfLife(rows, cols) {
-  this.rows = rows;
-  this.cols = cols;
-  this._lives = [];
-}
+class GameOfLife {
+  constructor(rows, cols) {
+    this.rows = rows
+    this.cols = cols
+    this._lives = []
+    this._next_lives = []
+  }
 
+  initBoard = () => {
+    this._lives = _.range(this.rows)
+      .map((r) => (
+        _.range(this.cols).map((c) => Math.round(Math.random()))
+      ))
+    this._next_lives = _.range(this.rows).map((r) => _.range(this.cols))
+  }
 
-GameOfLife.prototype.initBoard = function() {
-  var self = this;
-  self._lives = _.range(this.rows)
-    .map((r) => {
-      return _.range(this.cols)
-        .map((c) => {
-          return Math.round(Math.random());
-        })
-    });
-}
+  getLives = () => this._lives
 
+  nextRound = () => {
+    for(let x = 0; x < this.rows; x++) {
+      for(let y = 0; y < this.cols; y++) {
+        let nbCount = _([
+          this._lives[x-1] && this._lives[x-1][y-1],
+          this._lives[x-1] && this._lives[x-1][y],
+          this._lives[x-1] && this._lives[x-1][y+1],
+          this._lives[x][y-1],
+          this._lives[x][y+1],
+          this._lives[x+1] && this._lives[x+1][y-1],
+          this._lives[x+1] && this._lives[x+1][y],
+          this._lives[x+1] && this._lives[x+1][y+1],
+        ]).compact().sum()
 
-GameOfLife.prototype.getLives = function() {
-  return this._lives;
-}
-
-
-GameOfLife.prototype.nextRound = function() {
-  var self = this;
-  var lives = self.getLives();
-  var newLives = _.range(self.rows).map((x) => _.range(self.cols));
-
-  for(var x=0; x<self.rows; x++) {
-    for(var y=0; y<self.cols; y++) {
-      var nbCount = _([
-        lives[x-1] && lives[x-1][y-1],
-        lives[x-1] && lives[x-1][y],
-        lives[x-1] && lives[x-1][y+1],
-        lives[x][y-1],
-        lives[x][y+1],
-        lives[x+1] && lives[x+1][y-1],
-        lives[x+1] && lives[x+1][y],
-        lives[x+1] && lives[x+1][y+1],
-      ]).compact().sum();
-
-      if ((lives[x][y]===1 && _.includes([2,3], nbCount)) || lives[x][y]===0 && nbCount===3) {
-        newLives[x][y] = 1;
-      } else {
-        newLives[x][y] = 0;
+        if ((this._lives[x][y]===1 && _.includes([2,3], nbCount)) || this._lives[x][y]===0 && nbCount===3) {
+          this._next_lives[x][y] = 1
+        } else {
+          this._next_lives[x][y] = 0
+        }
       }
     }
-  }
 
-  self._lives = newLives;
-  newLives = undefined;
+    this._lives = this._next_lives
+    this._next_lives = _.range(this.rows).map((r) => _.range(this.cols))
+  }
 }
 
 
-function arrayToObject(arr) {
-  var obj = [];
+const Cell = (props) => (
+  <div className="cell" {...props}></div>
+)
 
-  for(var x=0; x<arr.length; x++) {
-    for(var y=0; y<arr[x].length; y++) {
-      obj.push({
-        x: x,
-        y: y,
-        v: arr[x][y]
-      });
+
+const Row = ({cells}) => (
+  <div className="row">
+    {
+      cells.map((c, k) => <Cell data={c} key={k}/>)
     }
-  }
-
-  return obj
-}
-
-
-class Cell extends Component {
-  render() {
-    return (
-      <div className="cell" {...this.props}></div>
-    )
-  }
-}
-
-
-class Row extends Component {
-  render() {
-    let cells = this.props.cells.map(function(c, k) {
-      return <Cell data={c} key={k}/>;
-    });
-
-    return (
-      <div className="row">{cells}</div>
-    )
-  }
-}
+  </div>
+)
 
 
 class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {rows: 30, cols: 30, delay: 500, lives: [], interval: undefined};
-    this.onRowsChange = this.onRowsChange.bind(this);
-    this.onColsChange = this.onColsChange.bind(this);
-    this.onDelayChange = this.onDelayChange.bind(this);
-    this.onPlay = this.onPlay.bind(this);
+  rows = 30
+  cols = 30
+  delay = 500
+  interval = undefined
+  lives = []
+
+  state = {loading: true}
+
+  onRowsChange = (e) => {
+    this.rows = parseInt(e.target.value) || ''
   }
 
-  onRowsChange(e) {
-    this.setState({rows: parseInt(e.target.value) || ''});
+  onColsChange = (e) => {
+    this.cols = parseInt(e.target.value) || ''
   }
 
-  onColsChange(e) {
-    this.setState({cols: parseInt(e.target.value) || ''});
+  onDelayChange = (e) => {
+    this.delay = parseInt(e.target.value) || ''
   }
 
-  onDelayChange(e) {
-    this.setState({delay: parseInt(e.target.value) || ''});
-  }
-
-  onPlay() {
-    if (this.state.interval) {
-      clearInterval(this.state.interval);
+  onPlay = () => {
+    if (this.interval) {
+      clearTimeout(this.interval)
     }
 
-    let game = new GameOfLife(this.state.rows, this.state.cols);
-    game.initBoard();
-    this.setState({lives: game.getLives()});
+    const game = new GameOfLife(this.rows, this.cols)
+    game.initBoard()
 
-    let itv = setInterval(()=>{
-      game.nextRound();
-      this.setState({lives: game.getLives()});
-    }, this.state.delay);
-    this.setState({interval: itv});
+    const handler = () => {
+      game.nextRound()
+      this.lives = game.getLives()
+      this.setState({loading: false})
+      this.interval = setTimeout(handler, this.delay)
+    }
+
+    handler()
   }
 
   render() {
-    let rows = this.state.lives.map(function(row, k) {
-      return <Row cells={row} key={k}/>;
-    });
-
     return (
       <div>
         <div>
           <label>Rows:</label>
-          <input id="rows" type="text" value={this.state.rows} onChange={this.onRowsChange}/>
+          <input id="rows" type="text" value={this.rows} onChange={this.onRowsChange}/>
           <label>Cols:</label>
-          <input id="cols" type="text" value={this.state.cols} onChange={this.onColsChange}/>
+          <input id="cols" type="text" value={this.cols} onChange={this.onColsChange}/>
           <label>Delay:</label>
-          <input id="delay" type="text" value={this.state.delay} onChange={this.onDelayChange}/>
+          <input id="delay" type="text" value={this.delay} onChange={this.onDelayChange}/>
           <button onClick={this.onPlay}>Play</button>
         </div>
         <div className="table">
-          {rows}
+          {
+            this.state.loading || this.lives.map((row, k) => <Row cells={row} key={k}/>)
+          }
         </div>
       </div>
-    );
+    )
   }
-};
+}
 
 
-render(<App/>, document.getElementById('board'));
-
+render(<App/>, document.getElementById('board'))
--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,12 @@ module.exports = {
     filename: 'gol.react.bundle.js'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: ['react', 'es2015']
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
       }
     ]
