8b0f740fba5d7e668780ecd0cb9b847a9335b14b
Zhang Yu <zhangyu@dingxiang-inc.com>
2019-06-27 22:31:23 +0800

    update d3 version to es5+ standard

--- gol.d3.js
+++ gol.d3.js
@@ -1,111 +1,91 @@
-"use strict";
-
-
-function GameOfLife(rows, cols) {
-  this.rows = rows;
-  this.cols = cols;
-  this._lives = [];
-}
-
-
-GameOfLife.prototype.initBoard = function() {
-  var self = this;
-  self._lives = _.range(this.rows)
-    .map((r) => {
-      return _.range(this.cols)
-        .map((c) => {
-          return Math.round(Math.random());
-        })
-    });
-}
-
-
-GameOfLife.prototype.getLives = function() {
-  return this._lives;
-}
-
-
-GameOfLife.prototype.nextRound = function() {
-  var self = this;
-  var lives = self.getLives();
-  var newLives = _.range(self.rows).map((x) => _.range(self.cols));
-
-  for(var x=0; x<self.rows; x++) {
-    for(var y=0; y<self.cols; y++) {
-      var nbCount = _([
-        lives[x-1] && lives[x-1][y-1],
-        lives[x-1] && lives[x-1][y],
-        lives[x-1] && lives[x-1][y+1],
-        lives[x][y-1],
-        lives[x][y+1],
-        lives[x+1] && lives[x+1][y-1],
-        lives[x+1] && lives[x+1][y],
-        lives[x+1] && lives[x+1][y+1],
-      ]).compact().sum();
-
-      if ((lives[x][y]===1 && _.includes([2,3], nbCount)) || lives[x][y]===0 && nbCount===3) {
-        newLives[x][y] = 1;
-      } else {
-        newLives[x][y] = 0;
-      }
-    }
+class GameOfLife {
+  constructor(rows, cols) {
+    this.rows = rows
+    this.cols = cols
+    this._lives = []
+    this._next_lives = []
   }
 
-  self._lives = newLives;
-  newLives = undefined;
-}
+  initBoard = () => {
+    this._lives = _.range(this.rows)
+      .map((r) => (
+        _.range(this.cols).map((c) => Math.round(Math.random()))
+      ))
+    this._next_lives = _.range(this.rows).map((r) => _.range(this.cols))
+  }
 
+  getLives = () => this._lives
 
-function Board(container) {
-  var chart = {left: 20, top: 20, r: 5, border: 2};
-  var svg = d3.select(container).append('svg');
+  nextRound = () => {
+    for(let x = 0; x < this.rows; x++) {
+      for(let y = 0; y < this.cols; y++) {
+        let nbCount = _([
+          this._lives[x-1] && this._lives[x-1][y-1],
+          this._lives[x-1] && this._lives[x-1][y],
+          this._lives[x-1] && this._lives[x-1][y+1],
+          this._lives[x][y-1],
+          this._lives[x][y+1],
+          this._lives[x+1] && this._lives[x+1][y-1],
+          this._lives[x+1] && this._lives[x+1][y],
+          this._lives[x+1] && this._lives[x+1][y+1],
+        ]).compact().sum()
 
-  function arrayToObject(arr) {
-    var obj = [];
-
-    for(var x=0; x<arr.length; x++) {
-      for(var y=0; y<arr[x].length; y++) {
-        obj.push({
-          x: x,
-          y: y,
-          v: arr[x][y]
-        });
+        if ((this._lives[x][y]===1 && _.includes([2,3], nbCount)) || this._lives[x][y]===0 && nbCount===3) {
+          this._next_lives[x][y] = 1
+        } else {
+          this._next_lives[x][y] = 0
+        }
       }
     }
 
-    return obj
+    this._lives = this._next_lives
+    this._next_lives = _.range(this.rows).map((r) => _.range(this.cols))
+  }
+}
+
+
+class Board {
+  constructor(container) {
+    this.chart = {left: 20, top: 20, r: 5, border: 2}
+    this.svg = d3.select(container).append('svg')
   }
 
-  chart.render = function(lives) {
-    var data = arrayToObject(lives);
+  arrayToObject = (arr) => (
+    _.flatten(_.range(arr.length).map((x) => (
+      _.range(arr[0].length).map((y) => {
+        return {x: x, y: y, v: arr[x][y]}
+      })
+    )))
+  )
 
-    svg.attr('height', (chart.r*2+chart.border)*lives.length+chart.top);
+  render = (lives) => {
+    const data = this.arrayToObject(lives)
+    console.log(data[0])
 
+    this.svg.attr('height', (this.chart.r * 2 + this.chart.border) * lives.length + this.chart.top)
     // Enter
-    svg.selectAll('circle')
+    this.svg.selectAll('circle')
       .data(data)
       .enter()
         .append('circle')
-          .attr('cx', function(d) {
-            return d.y * (chart.r * 2 + chart.border) + chart.left;
-          })
-          .attr('cy', function(d) {
-            return d.x * (chart.r * 2 + chart.border) + chart.top;
-          })
-          .attr('r', chart.r)
-          .attr('data', (d) => d.v);
+        .attr('cx', (d) => (
+            d.y * (this.chart.r * 2 + this.chart.border) + this.chart.left
+          ))
+        .attr('cy', (d) => (
+            d.x * (this.chart.r * 2 + this.chart.border) + this.chart.top
+          ))
+        .attr('r', this.chart.r)
+        .attr('data', (d) => d.v)
 
     // Update
-    svg.selectAll('circle')
+    this.svg.selectAll('circle')
       .data(data)
-        .attr('data', (d) => d.v);
+      .attr('data', (d) => d.v)
 
     // Exit
-    svg.selectAll('circle')
+    this.svg.selectAll('circle')
       .data(data)
       .exit()
-        .remove();
+        .remove()
   }
-
-  return chart;
 }
